2e39fa9f3785f081a06c122c5ddf87c78578b30c
skmonsterr <ctjoaomartins@gmail.com>
2022-12-01 00:26:57 -0300

    Add handle async errors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express';
+import "express-async-errors";
 import { routes } from './routes';
 
 const app = express();
@@ -6,7 +7,21 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+
+  if (err instanceof Error) {
+    return response.status(400).json({
+      message: err.message,
+    });
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error",
+  })
+})
+
 
 app.listen(3333, () => {
-  console.log('Server is running on PORT 3333 🚀 \nRedirect to your server: http://localhost:3333\n')
+  console.log('Server is running on PORT 3333 🚀')
 })
